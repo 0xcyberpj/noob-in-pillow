a3e25893f439bca4c04367a983a1d2ef3813253e
P4ul Jeremi4h <72292872+0xcyberpj@users.noreply.github.com>
2022-03-10 18:45:27 +0530

    Update filter.py

--- filter.py
+++ filter.py
@@ -1,7 +1,11 @@
-#!/usr/bin/python3
+'''#!/usr/bin/python3
 from PIL import Image
 from os import system as sis
 from os import sys
 img=sys.argv[1]
 file=Image.open(img)
 file.filter(Imagefilter.CONTOUR).show()
+
+'''
+
+
